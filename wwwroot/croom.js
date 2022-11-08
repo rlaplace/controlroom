1791cd948c59cc262e4fcb4bd0391bfc6a94a6a9
rlaplace <rodrigo7890@yahoo.com.br>
2022-11-08 15:15:27 -0300

    initial prototype under contruction

--- wwwroot/croom.js
+++ wwwroot/croom.js
@@ -6,12 +6,53 @@ var objDragged=null;
 var pntDragged=null;
 var oldX, oldY;
 var params=new Array();
+var uiNumberPickers;
+
+function plusButtonClick()
+{
+alert("S1");
+}
+
+function assemblyUiNumberPicker(element)
+{
+  while (element.hasChildNodes())
+    element.removeChild(element.firstChild);
+  var width=56;
+  var inputBox = document.createElementNS(svgNS,'path');
+  inputBox.setAttribute("d", "M 0.5 2.5 a 4 4 90 0 1 2 -2 h "+(width-4)+" a 4 4 90 0 1 2 2 v 18 a 4 4 90 0 1 -2 2 h -"+(width-4)+" a 4 4 90 0 1 -2 -2 z");
+  inputBox.setAttribute("fill", "#FFDDDD");
+  inputBox.setAttribute("stroke", "#220000");
+  inputBox.setAttribute("stroke-width", "1");
+  element.appendChild(inputBox);
+  var inputText = document.createElementNS(svgNS,'text');
+  inputText.textContent="250";
+  inputText.setAttribute("x", "5");
+  inputText.setAttribute("y", "16");
+  inputText.setAttribute("text-anchor", "left");
+  element.appendChild(inputText);
+  var plusButton = document.createElementNS(svgNS,'path');
+  plusButton.setAttribute("d", "M "+(width-10)+".5 4.5 a 4 4 90 0 1 2 -2 h 4 a 4 4 90 0 1 2 2 v 4 a 4 4 90 0 1 -2 2 h -4 a 4 4 90 0 1 -2 -2 z M "+(width-8)+".5 6.5 h 4 M "+(width-6)+".5 4.5 v 4");
+  plusButton.setAttribute("onclick", "plusButtonClick()");
+  plusButton.setAttribute("fill", "#FFDDDD");
+  plusButton.setAttribute("stroke", "#220000");
+  plusButton.setAttribute("stroke-width", "1");
+  element.appendChild(plusButton);
+  var minusButton = document.createElementNS(svgNS,'path');
+  minusButton.setAttribute("d", "M "+(width-10)+".5 14.5 a 4 4 90 0 1 2 -2 h 4 a 4 4 90 0 1 2 2 v 4 a 4 4 90 0 1 -2 2 h -4 a 4 4 90 0 1 -2 -2 z M "+(width-8)+".5 16.5 h 4 M "+(width-6)+".5 4.5 v 4");
+  minusButton.setAttribute("fill", "#FFDDDD");
+  minusButton.setAttribute("stroke", "#220000");
+  minusButton.setAttribute("stroke-width", "1");
+  element.appendChild(minusButton);
+}
 
 function init() {
   mainPanel = document.getElementById("mainpanel");
   editPanel = document.getElementById("editpanel");
   editItems = document.getElementById("edititems");
   menuPanel = document.getElementById("menupanel");
+  uiNumberPickers = document.getElementsByClassName("uiNumberPicker");
+  for(var i=0; i<uiNumberPickers.length; i++)
+    assemblyUiNumberPicker(uiNumberPickers[i]);
 }
 
 function touch(evt)
@@ -91,7 +132,7 @@ function drag(evt)
 	  var xey = params[1].split(/([,])/);
 	  var newx = parseFloat(xey[0].trim()) + dx;
 	  var newy = parseFloat(xey[2].trim()) + dy;
-	  params[1]= "" + newx + "," + newy;
+	  params[1]= "" + newx + " " + newy;
 	  objDragged.setAttribute("d", params.join(" "));
 	}
 	else if (objDragged.nodeName=="g")
@@ -127,11 +168,11 @@ function drop(evt)
 {
   objDragged=null;
   pntDragged=null;
-  while (editItems.hasChildNodes())
-    editItems.removeChild(editItems.firstChild);
   var realtarget=evt.target;
   while ((realtarget.parentNode!=mainPanel)&&(realtarget.parentNode!=null))
     realtarget=realtarget.parentNode;
+  var vtrue=true;
+  if (vtrue==false)
   for(var i=0; i<3; i++)
   {
     var newItem = document.createElementNS(svgNS,'g');
@@ -190,7 +231,7 @@ function setPosition(el, x, y) {
 function insertPath() {
   var newNode = document.createElementNS(svgNS,'path');
   setPosition(newNode, 70, 220);
-  newNode.setAttribute('d','M 0,0 l 70,-150 l 60,100 l 70,-150');
+  newNode.setAttribute('d','M 0 0 l 70 -150 l 60 100 l 70 -150');
   newNode.setAttribute('fill','none');
   newNode.setAttribute('stroke','black');
   newNode.setAttribute('stroke-linecap','round');
